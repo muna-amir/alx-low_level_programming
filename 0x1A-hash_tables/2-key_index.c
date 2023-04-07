2693a79b8a0c2785330036fee95f96d3ac2a33d7
muna-amir <maimuamir@gmail.com>
2023-04-07 12:58:20 -0700

    hash_tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,8 +8,9 @@
  * Return: index for the key
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size) {
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
   unsigned long int hash = hash_djb2(key);
   unsigned long int index = hash % size;
-  return index;
+  return (index);
 }
